0130b37aa885a2c6efbad17bcba34bb325e990a3
Empire of E <62396650+Empire-of-E-Projects@users.noreply.github.com>
2021-11-11 12:47:07 +1100

    Update index.php
    try server self

--- index.php
+++ index.php
@@ -292,7 +292,7 @@ function file_get($target)
 {
 global $testing;
 $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, dirname(__FILE__)."/get.php?url=".urlencode($target));
+  curl_setopt($ch, CURLOPT_URL, "https://" . $_SERVER['HTTP_HOST']."/get.php?url=".urlencode($target));
   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
   curl_setopt($ch, CURLOPT_TIMEOUT, 30);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
