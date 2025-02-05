e1ebbcd49d1cfde5e19bbf6b50fbddabd280a199
Kenasa101 <150551223+Kenasa101@users.noreply.github.com>
2025-02-05 17:55:30 +0300

    Update index.php

--- proxy/index.php
+++ proxy/index.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 // get target
-$url = "https://example.com";
+$url = "https://kenoexample.com";
 if(hasParam('url'))
 {
 $url = urldecode($_REQUEST['url']);
